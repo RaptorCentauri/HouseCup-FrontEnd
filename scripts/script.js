025afedc3428c499c2a2f571a388755b42cfa1bd
RaptorCentauri <dave.leo.shilander@icloud.com>
2020-10-02 16:33:00 -0500

    removed needless comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,11 @@
-console.log('running script js');
-
-// console.log(axios);
-// const corsAnywhere = `https://cors-anywhere.herokuapp.com/`
-// const houseCupAPI = 
-
-// axios.get(`${corsAnywhere}https://nu-chi-house-cup.herokuapp.com/api/students/`).then(resp=>{
-//     console.log(resp);
-// })
 
 
-console.log(axios);
+
 const corsAnywhere = `https://cors-anywhere.herokuapp.com/`
 const houseCupAPI = `https://nu-chi-house-cup.herokuapp.com/api/students/`
-// const localAPI = `http://localhost:7500/api/students`
-// const houseCupAPI = `http://dave.leo.shilander/api/projects`
 
 
-// axios.get(`${localAPI}`).then(resp => {
-    axios.get(`${corsAnywhere}${houseCupAPI}`).then(resp => {
+axios.get(`${corsAnywhere}${houseCupAPI}`).then(resp => {
 
     console.log(resp.data);
     let students = resp.data;
@@ -30,19 +18,9 @@ const houseCupAPI = `https://nu-chi-house-cup.herokuapp.com/api/students/`
     teams.winamp = students.filter(student => student.teamName === 'winamp');
     teams.xanga = students.filter(student => student.teamName === 'xanga'); 
 
-    // console.log('teamAOL', teamAOL);
-    // console.log('teamNapster', teamNapster);
-    // console.log('teamNetscape', teamNetscape);
-    // console.log('teamWinamp', teamWinamp);
-    // console.log('teamXanga', teamXanga);
 
     console.log(teams.aol);
 
-    // const reducer = () => {
-
-    // }
-
-    // let aolScore = teams.aol.reduce()
 
     for (const member of teams.aol) {
         console.log(member);
