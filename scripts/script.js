3b4534db86b31ae2e96539ed4fd184d8d30f48d8
RaptorCentauri <dave.leo.shilander@icloud.com>
2020-10-02 19:59:57 -0500

    using awaits, scores working

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,69 @@
 
 
 
-const corsAnywhere = `https://cors-anywhere.herokuapp.com/`
-const houseCupAPI = `https://nu-chi-house-cup.herokuapp.com/api/students/`
+const corsAnywhere = `https://cors-anywhere.herokuapp.com/`;
+const houseCupAPI = `https://nu-chi-house-cup.herokuapp.com/api/students/`;
+
+const scoresContainer = document.getElementById('scores-container');
+
+let displayScore = (name='Team Name', score = 0) => {
+    let teamLogo = document.createElement(`img`);
+    teamLogo.setAttribute('src',`./assets/${name}.png`)
+    teamLogo.setAttribute('class', 'team-logo');
 
 
-axios.get(`${corsAnywhere}${houseCupAPI}`).then(response => {
+    let teamElement = document.createElement(`h3`)
+    teamElement.textContent = `${name}: ${score}`
+    scoresContainer.append(teamLogo)
+    scoresContainer.append(teamElement);
+}
 
-    let students = response.data;
+let getTeams = async () => {
 
-    let teams  = {};
-    let scores = {}
+    let response = await axios.get(`${corsAnywhere}${houseCupAPI}`);
+    console.log(response);
+    let students = response.data
+    let teams = {};
 
-    teams.aol = students.filter(student => student.teamName === 'aol' )
+    teams.aol = students.filter(student => student.teamName === 'aol')
     teams.napster = students.filter(student => student.teamName === 'napster');
     teams.netscape = students.filter(student => student.teamName === 'netscape');
     teams.winamp = students.filter(student => student.teamName === 'winamp');
     teams.xanga = students.filter(student => student.teamName === 'xanga'); 
 
+    return teams
 
+}
+
+
+
+
+
+
+getTeams().then((teams)=>{
+    let teamScores = {}
 
     function getScores(teamName) {
-        scores[teamName] = teams[teamName].reduce((a, c) => a + c.pointsEarned, 0)
+        teamScores[teamName] = teams[teamName].reduce((a, c) => a + c.pointsEarned, 0)
     }
+
 
     for (const team in teams) {
         getScores(team)
     }
 
+    for (team in teamScores) {
+        console.log(teamScores[team]);
+        let score = teamScores[team]
+        displayScore(team, score );
+    }
 
-    console.log(scores);
+    
+    // console.log(scores);
+});
+
+
 
  
-
-})
 
   
